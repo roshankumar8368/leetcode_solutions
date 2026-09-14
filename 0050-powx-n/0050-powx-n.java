c04d7d8fac8d315a5d35ae44142c091eec11aa09
Roshan kumar <roshan.kumar.engineer11@gmail.com>
2026-09-14 22:32:10 +0530

    Time: 0 ms (100%), Space: 47.6 MB (77.32%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,32 @@
+class Solution {
+public double myPow(double x, int n) {
+
+    // Convert int to long because n can be Integer.MIN_VALUE
+    long power = n;
+    
+    // Handle negative power
+    if (power < 0) {
+        x = 1 / x;
+        power = -power;
+    }
+    
+    double result = 1;
+    
+    while (power > 0) {
+        
+        // If power is odd
+        if (power % 2 == 1) {
+            result = result * x;
+        }
+        
+        // Square the base
+        x = x * x;
+        
+        // Divide power by 2
+        power = power / 2;
+    }
+    
+    return result;
+}
+
+}
